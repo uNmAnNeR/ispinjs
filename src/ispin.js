ef3f016b11f63877c620af93abf18e7936bc4792
Kryazhev Alexey <alexey.kryazhev@bsc-ideas.com>
2018-02-21 10:18:30 +0300

    add flowtype

--- src/ispin.js
+++ src/ispin.js
@@ -1,7 +1,42 @@
+// @flow
+
+type SpinDirection = (1 | -1);
+type SpinOptions = {
+  wrapperClass: string,
+  buttonsClass: string,
+  step: number,
+  pageStep: number,
+  repeatInterval: number,
+  wrapOverflow: boolean,
+  parse: string => number,
+  format: number => string,
+  disabled: boolean,
+  max?: number,
+  min?: number,
+  onChange?: string => void,
+};
+
+
 export default
 class ISpin {
-  constructor (el, opts) {
+  static DEFAULTS: $Shape<SpinOptions>;
+  el: HTMLInputElement;
+  options: SpinOptions;
+  _onKeyDown: (e: KeyboardEvent) => void;
+  _onMouseDown: (e: MouseEvent) => void;
+  _onMouseUp: (e: MouseEvent) => void;
+  _onMouseLeave: (e: MouseEvent) => void;
+  _onWheel: (e: WheelEvent) => void;
+  _wrapper: HTMLElement;
+  _buttons: {
+    inc: HTMLButtonElement,
+    dec: HTMLButtonElement
+  };
+  _spinTimer: IntervalID;
+
+  constructor (el: HTMLInputElement, opts: $Shape<SpinOptions>) {
     this.el = el;
+    // $FlowFixMe temporary assignment
     this.options = {};
 
     this._onKeyDown = this._onKeyDown.bind(this);
@@ -17,7 +52,7 @@ class ISpin {
   build () {
     // wrap element
     this._wrapper = document.createElement('div');
-    this.el.parentNode.appendChild(this._wrapper);
+    if (this.el.parentNode) this.el.parentNode.appendChild(this._wrapper);
     this._wrapper.appendChild(this.el);
 
     // add buttons
@@ -39,7 +74,7 @@ class ISpin {
     this.el.addEventListener('wheel', this._onWheel);
   }
 
-  update (opts) {
+  update (opts: $Shape<SpinOptions>) {
     opts = {
       ...ISpin.DEFAULTS,
       ...this.options,
@@ -73,13 +108,13 @@ class ISpin {
   }
 
   destroy () {
-    this.el.parentNode.replaceChild(this.el, this._wrapper);
+    if (this.el.parentNode) this.el.parentNode.replaceChild(this.el, this._wrapper);
     delete this.el;
     delete this._wrapper;
     delete this._buttons;
   }
 
-  _onKeyDown (e) {
+  _onKeyDown (e: KeyboardEvent) {
     switch (e.keyCode) {
       case 38: // arrow up
         return this.spin(this.options.step);
@@ -92,10 +127,10 @@ class ISpin {
     }
   }
 
-  _onMouseDown (e) {
+  _onMouseDown (e: MouseEvent) {
     e.preventDefault();
 
-    const direction = e.currentTarget === this._buttons.inc ? 1 : -1;
+    const direction: SpinDirection = e.currentTarget === this._buttons.inc ? 1 : -1;
     this.spin(direction * this.options.step);
     this.el.focus();
 
@@ -110,7 +145,7 @@ class ISpin {
     this._stopSpinning();
   }
 
-  _startSpinning (direction) {
+  _startSpinning (direction: SpinDirection) {
     this._stopSpinning();
     this._spinTimer = setInterval(() => this.spin(direction * this.options.step), this.options.repeatInterval);
   }
@@ -119,41 +154,43 @@ class ISpin {
     clearInterval(this._spinTimer);
   }
 
-  _onWheel (e) {
+  _onWheel (e: WheelEvent) {
     if (document.activeElement !== this.el) return;
     e.preventDefault();
 
-    this.spin((e.deltaY > 0 ? -1 : 1) * this.options.step);
+    const direction: SpinDirection = (e.deltaY > 0 ? -1 : 1);
+    this.spin(direction * this.options.step);
   }
 
-  get value () {
+  get value (): number {
     return this.options.parse(this.el.value);
   }
 
-  set value (value) {
-    value = this.options.format(this.options.parse(value));
-    this.el.value = value;
-    if (this.options.onChange) this.options.onChange(value);
+  set value (value: number) {
+    const strValue = this.options.format(this.options.parse(String(value)));
+    this.el.value = strValue;
+    if (this.options.onChange) this.options.onChange(strValue);
   }
 
-  get disabled () {
+  get disabled (): boolean {
     return this._buttons.inc.disabled;
   }
 
-  set disabled (disabled) {
+  set disabled (disabled: boolean) {
     if (this.disabled === disabled) return;
 
     this._buttons.inc.disabled =
     this._buttons.dec.disabled = disabled;
   }
 
-  get precision () {
+  get precision (): number {
     return Math.max(...[this.options.step, this.options.min]
       .filter(v => v != null)
+      // $FlowFixMe already checked above
       .map(precision));
   }
 
-  adjustValue (value) {
+  adjustValue (value: number): number {
     value = Number(value.toFixed(this.precision));
 
     if (this.options.max != null && value > this.options.max) value = this.options.max;
@@ -162,7 +199,7 @@ class ISpin {
     return value;
   }
 
-  wrapValue (value) {
+  wrapValue (value: number): number {
     if (this.options.wrapOverflow &&
       this.options.max != null &&
       this.options.min != null
@@ -173,7 +210,7 @@ class ISpin {
     return value;
   }
 
-  spin (step) {
+  spin (step: number) {
     this.value = this.adjustValue(this.wrapValue(this.value + step));
   }
 }
@@ -182,12 +219,13 @@ ISpin.DEFAULTS = {
   buttonsClass: 'spin-button',
   step: 1,
   pageStep: 10,
+  disabled: false,
   repeatInterval: 200,
   wrapOverflow: false,
   parse: Number,
   format: String
 };
 
-function precision (num) {
+function precision (num: number) {
   return (String(num).split('.')[1] || '').length;
 }
